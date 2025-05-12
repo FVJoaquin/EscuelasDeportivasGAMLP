e0ab146f06e3029c4ffa0f57352053796d77aaac
Joaquin Flores <jfloresv5@fcpn.edu.bo>
2025-05-12 12:21:34 -0400

    cambios agregar Persona,Estudiante

---
+++ mavenproject1/src/main/java/mavenproject1/Estudiante.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mavenproject1;
+
+public class Estudiante extends Persona {
+private
+    String[] inscrip = new String[50];  //to Inscripcion
+    int nroInscrip;
+    String estado;
+public
+    Estudiante() {
+        nroInscrip=0;
+        estado="habilitado";
+    }
+//GETTERS - SETTERS
+    public String getInscrip(int i) {
+        return inscrip[i];
+    }
+    public void setInscrip(String ins,int i) {
+        nroInscrip++;
+        for(int j=nroInscrip;j>i;j--)
+            inscrip[j]=inscrip[j-1];
+        this.inscrip[i] = ins;
+    }
+    public int getNroInscrip() {
+        return nroInscrip;
+    }
+    public void setNroInscrip(int nroInscrip) {
+        this.nroInscrip = nroInscrip;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+//METODOS    
+public
+    void mostrar() {
+        super.mostrar();
+        System.out.println("estado... "+estado+" Inscripciones: "+nroInscrip);
+        for(int j=1;j<=nroInscrip;j++)
+            System.out.print(inscrip[j]);
+    }
+    
+    
+}
---
+++ mavenproject1/src/main/java/mavenproject1/Persona.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mavenproject1;
+
+public class Persona {
+protected
+    String nombre,paterno,materno,genero,observaciones;
+    int id,edad;
+public
+     Persona() {
+         nombre="juan";
+         paterno="perez";
+         materno="lopez";
+         genero="hombre";
+         observaciones="";
+         id=789456;
+         edad=15;
+     }
+// GETTERS - SETTERS
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getPaterno() {
+        return paterno;
+    }
+    public void setPaterno(String paterno) {
+        this.paterno = paterno;
+    }
+    public String getMaterno() {
+        return materno;
+    }
+    public void setMaterno(String materno) {
+        this.materno = materno;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    public String getObservaciones() {
+        return observaciones;
+    }
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+// METODOS
+public
+    void mostrar() {
+        System.out.println(nombre+" "+paterno+" "+materno+" CI:"+id+" edad: "+edad);
+        if(!observaciones.equals(""))
+            System.out.println("Observacion: "+observaciones);
+    }
+    
+    
+}
